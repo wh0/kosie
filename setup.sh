0faa15bf0a00ed017f9a94659cd2ef18966e772e
wh0 <wh0@users.noreply.github.com>
2015-06-14 20:20:00 -0700

    whatever it is, it's not working

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ chroot "$shmsys" adduser "$user" sudo
 
 cp -var --parents /opt/kite /etc/init/klient.conf /etc/init.d/klient /usr/share/doc/klient /etc/kite /etc/network/interfaces.d/*.cfg /etc/ssh/ssh_host_* "$shmsys"
 
-cat > /usr/share/initramfs-tools/scripts/local-bottom/kosie_move <<EOF
+cat > /usr/share/initramfs-tools/scripts/init-bottom/kosie_move <<EOF
 #!/bin/sh
 
 set -e
@@ -24,12 +24,20 @@ case "\$1" in
 	prereqs) exit 0;;
 esac
 
+mount -o remount,rw "\$rootmnt"
+cp "\$rootmnt"/initrd-backup.img "\$rootmnt"/initrd.img
+mount -o remount,ro "\$rootmnt"
+
+echo > /dev/kmsg "hello from kosie_move"
+
 shmroot=\$(mktemp -d)
 mount -t tmpfs none "\$shmroot"
 cp -a "\$rootmnt"$shmsys/* "\$shmroot"
 umount "\$rootmnt"
 mount -o move "\$shmroot" "\$rootmnt"
 rmdir "\$shmroot"
+
+echo > /dev/kmsg "goodbye from kosie_move"
 EOF
-chmod +x /usr/share/initramfs-tools/scripts/local-bottom/kosie_move
+chmod +x /usr/share/initramfs-tools/scripts/init-bottom/kosie_move
 update-initramfs -u
